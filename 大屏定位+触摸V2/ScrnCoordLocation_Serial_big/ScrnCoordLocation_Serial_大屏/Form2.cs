e2bb81079ae400fb1090899dbf8ceb0537ef95d1
Gao Shengxu <1059067158@qq.com>
2016-05-14 21:38:10 +0800

    Add a version which could verify the function of touch.And the button is  Metallic.

---
+++ 大屏定位+触摸V2/ScrnCoordLocation_Serial_big/ScrnCoordLocation_Serial_大屏/Form2.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace serialport
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1_1.BackColor == Color.Transparent)
+            {
+
+                button1_1.BackColor = Color.Red;
+            }
+            else if (button1_1.BackColor == Color.Red)
+            {
+                button1_1.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button1_2_Click(object sender, EventArgs e)
+        {
+            if (button1_2.BackColor == Color.Transparent)
+            {
+
+                button1_2.BackColor = Color.Red;
+            }
+            else if (button1_2.BackColor == Color.Red)
+            {
+                button1_2.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button1_3_Click(object sender, EventArgs e)
+        {
+            if (button1_3.BackColor == Color.Transparent)
+            {
+
+                button1_3.BackColor = Color.Red;
+            }
+            else if (button1_3.BackColor == Color.Red)
+            {
+                button1_3.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button1_4_Click(object sender, EventArgs e)
+        {
+            if (button1_4.BackColor == Color.Transparent)
+            {
+
+                button1_4.BackColor = Color.Red;
+            }
+            else if (button1_4.BackColor == Color.Red)
+            {
+                button1_4.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button1_5_Click(object sender, EventArgs e)
+        {
+            if (button1_5.BackColor == Color.Transparent)
+            {
+
+                button1_5.BackColor = Color.Red;
+            }
+            else if (button1_5.BackColor == Color.Red)
+            {
+                button1_5.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button2_1_Click(object sender, EventArgs e)
+        {
+            if (button2_1.BackColor == Color.Transparent)
+            {
+
+                button2_1.BackColor = Color.Red;
+            }
+            else if (button2_1.BackColor == Color.Red)
+            {
+                button2_1.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button2_2_Click(object sender, EventArgs e)
+        {
+            if (button2_2.BackColor == Color.Transparent)
+            {
+
+                button2_2.BackColor = Color.Red;
+            }
+            else if (button2_2.BackColor == Color.Red)
+            {
+                button2_2.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button2_3_Click(object sender, EventArgs e)
+        {
+            if (button2_3.BackColor == Color.Transparent)
+            {
+
+                button2_3.BackColor = Color.Red;
+            }
+            else if (button2_3.BackColor == Color.Red)
+            {
+                button2_3.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button2_4_Click(object sender, EventArgs e)
+        {
+            if (button2_4.BackColor == Color.Transparent)
+            {
+
+                button2_4.BackColor = Color.Red;
+            }
+            else if (button2_4.BackColor == Color.Red)
+            {
+                button2_4.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button2_5_Click(object sender, EventArgs e)
+        {
+            if (button2_5.BackColor == Color.Transparent)
+            {
+
+                button2_5.BackColor = Color.Red;
+            }
+            else if (button2_5.BackColor == Color.Red)
+            {
+                button2_5.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button3_1_Click(object sender, EventArgs e)
+        {
+            if (button3_1.BackColor == Color.Transparent)
+            {
+
+                button3_1.BackColor = Color.Red;
+            }
+            else if (button3_1.BackColor == Color.Red)
+            {
+                button3_1.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button3_2_Click(object sender, EventArgs e)
+        {
+            if (button3_2.BackColor == Color.Transparent)
+            {
+
+                button3_2.BackColor = Color.Red;
+            }
+            else if (button3_2.BackColor == Color.Red)
+            {
+                button3_2.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button3_3_Click(object sender, EventArgs e)
+        {
+            if (button3_3.BackColor == Color.Transparent)
+            {
+
+                button3_3.BackColor = Color.Red;
+            }
+            else if (button3_3.BackColor == Color.Red)
+            {
+                button3_3.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button3_4_Click(object sender, EventArgs e)
+        {
+            if (button3_4.BackColor == Color.Transparent)
+            {
+
+                button3_4.BackColor = Color.Red;
+            }
+            else if (button3_4.BackColor == Color.Red)
+            {
+                button3_4.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button3_5_Click(object sender, EventArgs e)
+        {
+            if (button3_5.BackColor == Color.Transparent)
+            {
+
+                button3_5.BackColor = Color.Red;
+            }
+            else if (button3_5.BackColor == Color.Red)
+            {
+                button3_5.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button4_1_Click(object sender, EventArgs e)
+        {
+            if (button4_1.BackColor == Color.Transparent)
+            {
+
+                button4_1.BackColor = Color.Red;
+            }
+            else if (button4_1.BackColor == Color.Red)
+            {
+                button4_1.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button4_2_Click(object sender, EventArgs e)
+        {
+            if (button4_2.BackColor == Color.Transparent)
+            {
+
+                button4_2.BackColor = Color.Red;
+            }
+            else if (button4_2.BackColor == Color.Red)
+            {
+                button4_2.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button4_3_Click(object sender, EventArgs e)
+        {
+            if (button4_3.BackColor == Color.Transparent)
+            {
+
+                button4_3.BackColor = Color.Red;
+            }
+            else if (button4_3.BackColor == Color.Red)
+            {
+                button4_3.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button4_4_Click(object sender, EventArgs e)
+        {
+            if (button4_4.BackColor == Color.Transparent)
+            {
+
+                button4_4.BackColor = Color.Red;
+            }
+            else if (button4_4.BackColor == Color.Red)
+            {
+                button4_4.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button4_5_Click(object sender, EventArgs e)
+        {
+            if (button4_5.BackColor == Color.Transparent)
+            {
+
+                button4_5.BackColor = Color.Red;
+            }
+            else if (button4_5.BackColor == Color.Red)
+            {
+                button4_5.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button5_1_Click(object sender, EventArgs e)
+        {
+            if (button5_1.BackColor == Color.Transparent)
+            {
+
+                button5_1.BackColor = Color.Red;
+            }
+            else if (button5_1.BackColor == Color.Red)
+            {
+                button5_1.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button5_2_Click(object sender, EventArgs e)
+        {
+            if (button5_2.BackColor == Color.Transparent)
+            {
+
+                button5_2.BackColor = Color.Red;
+            }
+            else if (button5_2.BackColor == Color.Red)
+            {
+                button5_2.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button5_3_Click(object sender, EventArgs e)
+        {
+            if (button5_3.BackColor == Color.Transparent)
+            {
+
+                button5_3.BackColor = Color.Red;
+            }
+            else if (button5_3.BackColor == Color.Red)
+            {
+                button5_3.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button5_4_Click(object sender, EventArgs e)
+        {
+            if (button5_4.BackColor == Color.Transparent)
+            {
+
+                button5_4.BackColor = Color.Red;
+            }
+            else if (button5_4.BackColor == Color.Red)
+            {
+                button5_4.BackColor = Color.Transparent;
+            }
+        }
+
+        private void button5_5_Click(object sender, EventArgs e)
+        {
+            if (button5_5.BackColor == Color.Transparent)
+            {
+
+                button5_5.BackColor = Color.Red;
+            }
+            else if (button5_5.BackColor == Color.Red)
+            {
+                button5_5.BackColor = Color.Transparent;
+            }
+        }
+
+
+
+      
+        private void userControl12_Click(object sender, EventArgs e)
+        {
+            if(BtnTest6_1.ButtonColor == Color.DimGray)
+            {
+               BtnTest6_1.BackColor = Color.Red;
+               BtnTest6_1.GlowColor = Color.Red;
+               BtnTest6_1.ButtonColor = Color.White;
+            }
+            else if (BtnTest6_1.BackColor == Color.Red)
+            {
+                BtnTest6_1.BackColor = Color.Transparent;
+                BtnTest6_1.GlowColor = Color.Transparent;
+                BtnTest6_1.ButtonColor = Color.DimGray;    
+            }
+
+
+        }
+
+        private void BtnTest6_2_Click(object sender, EventArgs e)
+        {
+            if (BtnTest6_2.ButtonColor == Color.DimGray)
+            {
+                BtnTest6_2.BackColor = Color.Red;
+                BtnTest6_2.GlowColor = Color.Red;
+                BtnTest6_2.ButtonColor = Color.White;
+            }
+            else if (BtnTest6_2.BackColor == Color.Red)
+            {
+                BtnTest6_2.BackColor = Color.Transparent;
+                BtnTest6_2.GlowColor = Color.Transparent;
+                BtnTest6_2.ButtonColor = Color.DimGray;
+            }
+        }
+
+        private void BtnTest6_3_Click(object sender, EventArgs e)
+        {
+            if (BtnTest6_3.ButtonColor == Color.DimGray)
+            {
+                BtnTest6_3.BackColor = Color.Red;
+                BtnTest6_3.GlowColor = Color.Red;
+                BtnTest6_3.ButtonColor = Color.White;
+            }
+            else if (BtnTest6_3.BackColor == Color.Red)
+            {
+                BtnTest6_3.BackColor = Color.Transparent;
+                BtnTest6_3.GlowColor = Color.Transparent;
+                BtnTest6_3.ButtonColor = Color.DimGray;
+            }
+        }
+
+        private void BtnTest6_4_Click(object sender, EventArgs e)
+        {
+
+            if (BtnTest6_4.ButtonColor == Color.DimGray)
+            {
+                BtnTest6_4.BackColor = Color.Red;
+                BtnTest6_4.GlowColor = Color.Red;
+                BtnTest6_4.ButtonColor = Color.White;
+            }
+            else if (BtnTest6_4.BackColor == Color.Red)
+            {
+                BtnTest6_4.BackColor = Color.Transparent;
+                BtnTest6_4.GlowColor = Color.Transparent;
+                BtnTest6_4.ButtonColor = Color.DimGray;
+            }
+        }
+
+        private void BtnTest6_5_Click(object sender, EventArgs e)
+        {
+
+            if (BtnTest6_5.ButtonColor == Color.DimGray)
+            {
+                BtnTest6_5.BackColor = Color.Red;
+                BtnTest6_5.GlowColor = Color.Red;
+                BtnTest6_5.ButtonColor = Color.White;
+            }
+            else if (BtnTest6_5.BackColor == Color.Red)
+            {
+                BtnTest6_5.BackColor = Color.Transparent;
+                BtnTest6_5.GlowColor = Color.Transparent;
+                BtnTest6_5.ButtonColor = Color.DimGray;
+            }
+        }
+
+    
+
+
+
+  
+
+
+
+
+
+
+
+    }
+}
